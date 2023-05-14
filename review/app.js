45a7905166d1ab45f3304549a73d4fdc323de34a
Paulo <paulocorreapc93@gmail.com>
2023-05-14 15:05:08 -0400

    declaradas variaveis importadas do html

--- review/app.js
+++ review/app.js
@@ -29,4 +29,7 @@ const reviews = [
   },
 ];
 
-const img = document.getElementById('person-img')
+const img = document.getElementById("person-img");
+const author = document.getElementById("author");
+const job = document.getElementById("job");
+const info = document.getElementById("info");
