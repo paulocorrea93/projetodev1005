b5c98574d459a7fd8bc59f0c028b3dd0a42d90b3
Paulo <paulocorreapc93@gmail.com>
2023-05-14 15:01:23 -0400

    criado array com bando de dados dos usuários que fizeram review

---
+++ review/app.js
@@ -0,0 +1,30 @@
+const reviews = [
+  {
+    id: 1,
+    name: "ariana",
+    job: "software engennier",
+    image: "https://images2.imgbox.com/e0/57/qI5bbwvg_o.jpeg",
+    text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+  },
+  {
+    id: 2,
+    name: "anna",
+    job: "software engennier",
+    image: "https://images2.imgbox.com/e0/57/qI5bbwvg_o.jpeg",
+    text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+  },
+  {
+    id: 3,
+    name: "peter",
+    job: "software engennier",
+    image: "https://images2.imgbox.com/e0/57/qI5bbwvg_o.jpeg",
+    text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+  },
+  {
+    id: 4,
+    name: "bill",
+    job: "software engennier",
+    image: "https://images2.imgbox.com/e0/57/qI5bbwvg_o.jpeg",
+    text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+  },
+];
