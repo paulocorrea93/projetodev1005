04d0d10cd39b93c381b40ccad362d8bab1527c38
Paulo <paulocorreapc93@gmail.com>
2023-05-16 12:57:23 -0400

    correção de bug: no 'classList' a classe não precisa do '.' (ponto), antes do nome da classe

--- sideBar/app.js
+++ sideBar/app.js
@@ -3,9 +3,9 @@ const close = document.querySelector(".close-btn");
 const sidebar = document.querySelector(".sidebar");
 
 toggle.addEventListener("click", () => {
-  sidebar.classList.toggle(".show-sidebar");
+  sidebar.classList.toggle("show-sidebar");
 });
 
 close.addEventListener("click", () => {
-  sidebar.classList.remove(".show-sidebar");
+  sidebar.classList.remove("show-sidebar");
 });
