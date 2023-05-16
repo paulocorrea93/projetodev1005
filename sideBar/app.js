55f4f5df2c5dbed43f844e0c4c95985fd843e516
Paulo <paulocorreapc93@gmail.com>
2023-05-16 12:50:40 -0400

    lógica em javascript implementada versão 1.0

---
+++ sideBar/app.js
@@ -0,0 +1,11 @@
+const toggle = document.querySelector(".sidebar-toggle");
+const close = document.querySelector(".close-btn");
+const sidebar = document.querySelector(".sidebar");
+
+toggle.addEventListener("click", () => {
+  sidebar.classList.toggle(".show-sidebar");
+});
+
+close.addEventListener("click", () => {
+  sidebar.classList.remove(".show-sidebar");
+});
