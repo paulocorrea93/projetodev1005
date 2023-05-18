ccde47fd17d93b59ab35397df6339977e2eef5d5
Paulo <paulocorreapc93@gmail.com>
2023-05-18 16:51:18 -0400

    correção de bug: callback 'question' não havia sido atribuído ao 'questions.forEach'

--- questions/app.js
+++ questions/app.js
@@ -1,2 +1,13 @@
 const questions = document.querySelectorAll('.question');
 
+questions.forEach((question) => {
+    const btn = question.querySelectorAll('.question-btn')
+
+    btn.addEventListener('click', () => {
+        questions.forEach((item) => {
+            if(item !== question){
+                
+            }
+        })
+    })
+})
