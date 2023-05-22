6ec02bd2d391d5347d692285bb52672215e83473
Paulo <paulocorreapc93@gmail.com>
2023-05-22 14:21:33 -0400

    finalizada versão 1.0 da lógica do projeto 'video'

--- video/app.js
+++ video/app.js
@@ -5,5 +5,14 @@ btn.addEventListener("click", () => {
   if (!btn.classList.contains("slide")) {
     btn.classList.add("slide");
     video.pause();
+  } else {
+    btn.classList.remove("slide");
+    video.play();
   }
 });
+
+const preloader = document.querySelector('.preloader')
+
+window.addEventListener('load', () => {
+    preloader.classList.add('hide-preloader')
+})
