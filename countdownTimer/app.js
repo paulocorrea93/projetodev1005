193d95fc0032fa086c47c2170e7258e4bc4f297d
Paulo <paulocorreapc93@gmail.com>
2023-05-27 18:03:22 -0400

    adicionadas as datas 'ano, mes, dia, dia da semana, hora e minuto'  a serem atualizadas

--- countdownTimer/app.js
+++ countdownTimer/app.js
@@ -31,3 +31,15 @@ let tempDate = new Date();
 let tempYear = tempDate.getFullYear();
 let tempMonth = tempDate.getMonth();
 let tempDay = tempDate.getDay();
+
+const futureDate = new Date(tempYear, tempMonth, tempDay + 10, 11, 30, 0);
+
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+
+let month = futureDate.getMonth();
+month = months[month];
+const weekday = weekday[futureDate.getDay()];
+const date = futureDate.getDate;
+giveaways.textContent = `giveways ends on ${weekday}, ${date}, ${month}, ${year}, ${hours}:${minutes}`;
