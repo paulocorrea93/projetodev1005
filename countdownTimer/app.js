311a382f0427393ef00ba8184f5cd675b3f9a1c3
Paulo <paulocorreapc93@gmail.com>
2023-05-29 13:57:45 -0400

    correção de bug: variável 'countdown = setTimeInterval' para 'countdown = setInterval'

--- countdownTimer/app.js
+++ countdownTimer/app.js
@@ -23,9 +23,9 @@ const weekdays = [
   "Domingo",
 ];
 
-const giveaway = document.querySelector('.giveaway');
-const deadline = document.querySelector('.deadline');
-const items = document.querySelectorAll('.deadline-format h4');
+const giveaway = document.querySelector(".giveaway");
+const deadline = document.querySelector(".deadline");
+const items = document.querySelectorAll(".deadline-format h4");
 
 let tempDate = new Date();
 let tempYear = tempDate.getFullYear();
@@ -54,31 +54,30 @@ const getRemindingTime = () => {
   const oneHour = 60 * 60 * 1000;
   const oneMinute = 60 * 1000;
 
-  let days = t / oneDay
+  let days = t / oneDay;
 
-  days = Math.floor(days)
-  let hours = Math.floor((t % oneDay) / oneHour)
-  let minutes = Math.floor ((t % oneHour) / oneMinute)
-  let seconds = Math.floor((t % oneMinute) / 1000)
+  days = Math.floor(days);
+  let hours = Math.floor((t % oneDay) / oneHour);
+  let minutes = Math.floor((t % oneHour) / oneMinute);
+  let seconds = Math.floor((t % oneMinute) / 1000);
 
-  const values = [days, hours, minutes, seconds]
+  const values = [days, hours, minutes, seconds];
   const format = (item) => {
-    if (item < 10){
-      return (item = `0${item}`)
+    if (item < 10) {
+      return (item = `0${item}`);
     }
     return item;
-  }
+  };
 
   items.forEach((item, index) => {
-    item.innerHTML = format.apply(values[index])
-  })
+    item.innerHTML = format(values[index]);
+  });
 
-  if(t < 0){
-    clearInterval(countdown)
-    deadline.innerHTML = `<h4 class="expired"> sorry, this giveaway has expired! </h4>`
+  if (t < 0) {
+    clearInterval(countdown);
+    deadline.innerHTML = `<h4 class="expired">sorry, this giveaway has expired!</h4>`;
   }
-
 };
 
-let countdown = setTimeInterval(getRemindingTime, 1000);
-getRemindingTime() 
+let countdown = setInterval(getRemindingTime, 1000);
+getRemindingTime();
