a9c807e96bee092da361b72cfc903771b1ee47aa
Paulo <paulocorreapc93@gmail.com>
2023-06-02 15:39:18 -0400

    itens do DOM selecionados para serem usados na lógica

---
+++ todolist/app.js
@@ -0,0 +1,13 @@
+// seleção de itens
+
+const form = document.querySelector(".grocery-form");
+const alert = document.querySelector(".alert");
+const grocery = document.getElementById("grocery");
+const submitBtn = document.querySelector(".submit-btn");
+const container = document.querySelector(".grocery-container");
+const list = document.querySelector(".grocery-list");
+const clearBtn = document.querySelector(".clear-btn");
+
+let editElement;
+let editFlag = false;
+let editID = "";
