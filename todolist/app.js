6f2cabe072cfe7e85707ea5d36296a6281b20ddd
Paulo <paulocorreapc93@gmail.com>
2023-06-04 21:14:05 -0400

    criada condição para editar item na ToDo List

--- todolist/app.js
+++ todolist/app.js
@@ -1,4 +1,4 @@
-i// seleção de itens
+i; // seleção de itens
 
 const form = document.querySelector(".grocery-form");
 const alert = document.querySelector(".alert");
@@ -24,17 +24,17 @@ clearBtn.addEventListener("click", clearItems);
 window.addEventListener("DOMContentLoaded", setupItems);
 
 const addItem = (e) => {
-    e.preventDefault()
-    const value = grocery.value;
-    const id = new Date().getTime().toString();
+  e.preventDefault();
+  const value = grocery.value;
+  const id = new Date().getTime().toString();
 
-    if(value !== "" && !editFlag){
-        const element = document.createElement("article")
-        let attr = document.createAttribute("data-id")
-        attr.value = id;
-        element.setAttribbuteNode(attr);
-        element.classList.add("grocery-item");
-        element.innerHTML = `<p class="title"> ${value}</p>
+  if (value !== "" && !editFlag) {
+    const element = document.createElement("article");
+    let attr = document.createAttribute("data-id");
+    attr.value = id;
+    element.setAttribbuteNode(attr);
+    element.classList.add("grocery-item");
+    element.innerHTML = `<p class="title"> ${value}</p>
         <div class="btn-container">
         <button type="button" class="edit-btn">
         <i class=fas fa-edit"></i>
@@ -42,20 +42,27 @@ const addItem = (e) => {
         <button type="button" class="delete-btn">
         <i class="fas fa-trash"></i>
         </button>
-        </div>`
+        </div>`;
 
-        // eventos de click nos botões de editar e deletar
-    
-        const deleteBtn = element.querySelector('.delete-btn')
-        deleteBtn.addEventListener('click', deleteItem)
-        const editBtn = element.querySelector('.edit-btn')
-        editBtn.addEventListener('click', editItem)
-    
-        list.appendChild(element)
-        displayAlert("item adicionado a lista", "sucesso")
-        container.classList.add("show-container")
-        addToLocalStorage(id, value)
-        setBackToDefault()
-    }
+    // eventos de click nos botões de editar e deletar
 
-}
+    const deleteBtn = element.querySelector(".delete-btn");
+    deleteBtn.addEventListener("click", deleteItem);
+    const editBtn = element.querySelector(".edit-btn");
+    editBtn.addEventListener("click", editItem);
+
+    list.appendChild(element);
+    displayAlert("item adicionado a lista", "sucesso");
+    container.classList.add("show-container");
+    addToLocalStorage(id, value);
+    setBackToDefault();
+  } else if (value !== "" && editFlag) {
+    editElement.innerHTML = value;
+    displayAlert("valor alterado", "sucesso");
+
+    // editar no 'banco de dados' fake
+
+    editLocalStorage(editID, value);
+    setBackToDefault();
+  }
+};
