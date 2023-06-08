76bd29c27ffd97f29964a30ec1b8ec11d73bb7bc
Paulo <paulocorreapc93@gmail.com>
2023-06-08 15:16:21 -0400

    criada lógica para botões de avançar e retroceder

--- slide/app.js
+++ slide/app.js
@@ -1,4 +1,18 @@
-const slides = document.querySelector('.slide')
-const nextBtn = document.querySelector('.netxBtn')
-const prevBtn = document.querySelector('.prevBtn')
+const slides = document.querySelector(".slide");
+const nextBtn = document.querySelector(".netxBtn");
+const prevBtn = document.querySelector(".prevBtn");
 
+slides.forEach((slide, index) => {
+  slide.style.left = `${index * 100}%`;
+});
+
+let counter = 0;
+nextBtn.addEventListener("click", () => {
+  counter++;
+  carousel();
+});
+
+prevBtn.addEventListener("click", () => {
+  counter--;
+  carousel();
+});
