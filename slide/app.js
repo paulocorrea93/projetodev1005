e7dc87d373be7214433cea7265839d29cfa22d0e
Paulo <paulocorreapc93@gmail.com>
2023-06-08 15:29:40 -0400

    correção de bug: na declaração da variavel 'slides' é querySelectorAll em inves de querySelector

--- slide/app.js
+++ slide/app.js
@@ -1,11 +1,9 @@
-const slides = document.querySelector(".slide");
+const slides = document.querySelectorAll(".slide");
 const nextBtn = document.querySelector(".nextBtn");
 const prevBtn = document.querySelector(".prevBtn");
-
-slides.forEach((slide, index) => {
+slides.forEach(function (slide, index) {
   slide.style.left = `${index * 100}%`;
 });
-
 let counter = 0;
 nextBtn.addEventListener("click", () => {
   counter++;
@@ -18,21 +16,19 @@ prevBtn.addEventListener("click", () => {
 });
 
 const carousel = () => {
-    if(counter < slides.length - 1){
-        nextBtn.style.display = "block"
-    } else {
-        nextBtn.style.display = "none"
-    }
-
-    if(counter > 0){
-        prevBtn.style.display = "block"
-    } else {
-        prevBtn.style.display = "none"
-    }
-
-    slides.forEach((slide) => {
-        slide.style.transform = `translateX(-${counter * 100}%)`
-    })
+    if (counter < slides.length - 1) {
+    nextBtn.style.display = "block";
+  } else {
+    nextBtn.style.display = "none";
+  }
+  if (counter > 0) {
+    prevBtn.style.display = "block";
+  } else {
+    prevBtn.style.display = "none";
+  }
+  slides.forEach(function (slide) {
+    slide.style.transform = `translateX(-${counter * 100}%)`;
+  });
 }
 
 prevBtn.style.display = "none";
