d55e928387f0f7d7376c5a61fcc03cc549b69e88
Paulo <paulocorreapc93@gmail.com>
2023-06-10 16:36:41 -0400

    adicionada função para adicionar nova data

--- darkmode/app.js
+++ darkmode/app.js
@@ -4,3 +4,18 @@ const articlesContainer = document.querySelector(".articles");
 toggleBtn.addEventListener("click", () => {
   document.documentElement.classList.toggle("dark-theme");
 });
+
+const articlesData = articles.map((article) =>{
+    const {title, date, length, snippet} = article;
+    const formatDate = moment(date).format('MMMM Do, YYYY')
+    return `<article class="post">
+    <h2>${title}</h2>
+    <div class='post-info'">
+    <span>${formatDate}</span>
+    <span>${length} min read</span>
+    </div>
+    <p>
+    ${snippet}
+    </p>
+    </article>`
+})
