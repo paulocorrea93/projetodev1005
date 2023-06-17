e3fd47f468cd86eb3c9e501b7d441dbf30d3e1b4
Paulo <paulocorreapc93@gmail.com>
2023-06-16 23:42:26 -0400

    importados os arquivos específicos

--- pauloCorreaStore/app.js
+++ pauloCorreaStore/app.js
@@ -2,3 +2,7 @@ import './src/toggleSidebar.js'
 import './src/cart/toggleCart.js'
 import './src/cart/setupCart.js'
 
+import fetchProducts from './src/fetchProducts.js'
+import {setupStore, store} from './src/store.js'
+import display from './src/displayProducts.js'
+import getElements from './src/util.js'
