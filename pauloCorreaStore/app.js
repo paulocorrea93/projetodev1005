b49fcf39b871773f868938005de64326502686fa
Paulo <paulocorreapc93@gmail.com>
2023-06-16 23:46:46 -0400

    criada função assincrona para receber informação de produtos adicionados

--- pauloCorreaStore/app.js
+++ pauloCorreaStore/app.js
@@ -6,3 +6,12 @@ import fetchProducts from './src/fetchProducts.js'
 import {setupStore, store} from './src/store.js'
 import display from './src/displayProducts.js'
 import getElements from './src/util.js'
+
+const init = async () =>{
+    const products = await fetchProducts()
+    if(products){
+        setupStore(products)
+        const featured =    store.filter((product) => product.featured === true)
+        display(featured, getElement('.featured-center'))
+    }
+}
