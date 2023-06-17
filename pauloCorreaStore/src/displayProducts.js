0ee39db91f40f6d8bf9744aec577efedd432da60
Paulo <paulocorreapc93@gmail.com>
2023-06-17 10:42:25 -0400

    adicionado evento para adicionar ao carrinho caso o 'target' tenha a classe 'product-cart-btn'

--- pauloCorreaStore/src/displayProducts.js
+++ pauloCorreaStore/src/displayProducts.js
@@ -1,10 +1,11 @@
-import {formatPrice} from './util.js'
-import {addToCart} from './cart/setupCart.js'
+import { formatPrice } from "./util.js";
+import { addToCart } from "./cart/setupCart.js";
 
-const display = (products, elements, filters) => {
-    elements.innerHTML = products.map((product) => {
-        const {id, name, image, price} = product
-        return `<article class="product">
+const display = (products, element, filters) => {
+  element.innerHTML = products
+    .map((product) => {
+      const { id, name, image, price } = product;
+      return `<article class="product">
         <div class="product-container">
         <img src="${image} class="product-img img" alt="${name}"/>
         <div class="product-icons">
@@ -20,6 +21,16 @@ const display = (products, elements, filters) => {
         <p class="product-name">${name}</p>
         <h4 class="product-price">${formatPrice(price)}</h4>
         </footer>
-        </article>`
+        </article>`;
     })
-}
+    .join("");
+
+  if (filters) return;
+
+  element.addEventListener("click", (e) => {
+    const parent = e.target.parentElement;
+    if (parent.classList.contains("product-cart-btn")) {
+      addToCart(parent.dataset.id);
+    }
+  });
+};
