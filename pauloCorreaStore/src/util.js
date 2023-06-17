44afdbc74a42bb2a78f3d4c67fa28f3d88aa3ced
Paulo <paulocorreapc93@gmail.com>
2023-06-17 10:59:51 -0400

    criada função para selecionar elemento e para exibir erro caso ele não exista

--- pauloCorreaStore/src/util.js
+++ pauloCorreaStore/src/util.js
@@ -1,3 +1,9 @@
 const allProductsUrl = 'https://course-api.com/javascript-store-products'
 const singleProductUrl = 'https://course-api.com/javascript-store-single-product'
 
+const getElement = (selection) => {
+    const element = document.querySelector(selection)
+    if(element) return element
+    throw new Error (`Please, check "${selection}" selector, no such element exist`)
+}
+
