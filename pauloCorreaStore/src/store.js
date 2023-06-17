fa2abd64ef46f625ce2c5755562056c257c1e92c
Paulo <paulocorreapc93@gmail.com>
2023-06-17 10:50:36 -0400

    criada função para determinar campos de nome, preço, cores, etc

---
+++ pauloCorreaStore/src/store.js
@@ -0,0 +1,14 @@
+import { getStorageItem, setStorageItem } from "./util.js";
+
+let store = getStorageItem("store");
+const setupStore = (products) => {
+  store = products.map((product) => {
+    const {
+      id,
+      fields: { featured, name, price, company, colors, image:img },
+    } = product;
+    const image = img[0].thumbnails.large.url
+    return {id, featured, name, price, company, colors, image}
+  });
+  setStorageItem('store', store)
+};
