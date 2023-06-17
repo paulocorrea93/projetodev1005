a1c059d6d27ec2eebc28311e569b2e345e4c12cb
Paulo <paulocorreapc93@gmail.com>
2023-06-17 11:02:20 -0400

    criada função para colocar preço formatado para o padrão do dolar americano

--- pauloCorreaStore/src/util.js
+++ pauloCorreaStore/src/util.js
@@ -1,9 +1,19 @@
-const allProductsUrl = 'https://course-api.com/javascript-store-products'
-const singleProductUrl = 'https://course-api.com/javascript-store-single-product'
+const allProductsUrl = "https://course-api.com/javascript-store-products";
+const singleProductUrl =
+  "https://course-api.com/javascript-store-single-product";
 
 const getElement = (selection) => {
-    const element = document.querySelector(selection)
-    if(element) return element
-    throw new Error (`Please, check "${selection}" selector, no such element exist`)
-}
+  const element = document.querySelector(selection);
+  if (element) return element;
+  throw new Error(
+    `Please, check "${selection}" selector, no such element exist`
+  );
+};
 
+const formatPrice = (price) => {
+  let formattedPrice = new Intl.NumberFormar("en-US", {
+    style: "currency",
+    currency: "USD",
+  }).format((price / 100).toFixed(2));
+  return formattedPrice;
+};
