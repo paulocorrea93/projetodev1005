a990ddeadffb36be90c29e17d070d960d6208cee
Paulo <paulocorreapc93@gmail.com>
2023-06-19 10:42:58 -0400

    importação de elementos específicos para página 'product'

--- pauloCorreaStore/src/pages/product.js
+++ pauloCorreaStore/src/pages/product.js
@@ -3,3 +3,9 @@
 import "../toggleSidebar.js";
 import "../cart/toggleCart.js";
 import "../cart/setupCart.js";
+
+// especificos
+
+import {addToCart} from '../cart/setupCart.js'
+import { singleProductUrl, getElement, formatPrice } from "../util.js";
+
