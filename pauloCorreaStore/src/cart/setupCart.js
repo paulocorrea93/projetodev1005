9150727682d46a62eb46845d947135d2e4bf207c
Paulo <paulocorreapc93@gmail.com>
2023-06-19 11:09:59 -0400

    criado evento para mostrar carrinho de compras

--- pauloCorreaStore/src/cart/setupCart.js
+++ pauloCorreaStore/src/cart/setupCart.js
@@ -3,3 +3,7 @@ import {getElement} from '../util.js'
 const cartOverlay = getElement('.cart-overlay')
 const closeCartBtn = getElement('.cart-close')
 const toggleCartBtn = getElement('.toggle-cart')
+
+toggleCartBtn.addEventListener('click', () => {
+    cartOverlay.classList.add('show')
+})
