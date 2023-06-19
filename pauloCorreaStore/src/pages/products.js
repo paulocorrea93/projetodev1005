568b1afd26090278785498b107d992288d299057
Paulo <paulocorreapc93@gmail.com>
2023-06-18 23:40:28 -0400

    importadas as configurações especificas da página 'products'

--- pauloCorreaStore/src/pages/products.js
+++ pauloCorreaStore/src/pages/products.js
@@ -3,3 +3,16 @@
 import '../toggleSidebar.js'
 import '../cart/toggleCart.js'
 import '../cart/setupCart.js'
+
+// importação de filtros
+
+import setupSearch from '../filters/search.js'
+import setupCompanies from '../filters/companies.js'
+import setupPrice from '../filters/price.js'
+
+// importações especificas
+
+import {store, setupStore} from '../store.js'
+import display from '../displayProducts.js'
+import {getElement} from '../util.js'
+
