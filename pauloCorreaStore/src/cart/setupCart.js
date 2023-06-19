299131740aef4b87eea131cc8003dc19cae8c113
Paulo <paulocorreapc93@gmail.com>
2023-06-19 11:09:29 -0400

    adicionados elementos do DOM

---
+++ pauloCorreaStore/src/cart/setupCart.js
@@ -0,0 +1,5 @@
+import {getElement} from '../util.js'
+
+const cartOverlay = getElement('.cart-overlay')
+const closeCartBtn = getElement('.cart-close')
+const toggleCartBtn = getElement('.toggle-cart')
