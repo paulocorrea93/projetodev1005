bda099a967dd76acf1bcf8a54d18f8c43de81e63
Paulo <paulocorreapc93@gmail.com>
2023-06-18 23:44:06 -0400

    criada função assincrona para carregar a página de produtos

--- pauloCorreaStore/src/pages/products.js
+++ pauloCorreaStore/src/pages/products.js
@@ -1,18 +1,26 @@
 // importações globais
 
-import '../toggleSidebar.js'
-import '../cart/toggleCart.js'
-import '../cart/setupCart.js'
+import "../toggleSidebar.js";
+import "../cart/toggleCart.js";
+import "../cart/setupCart.js";
 
 // importação de filtros
 
-import setupSearch from '../filters/search.js'
-import setupCompanies from '../filters/companies.js'
-import setupPrice from '../filters/price.js'
+import setupSearch from "../filters/search.js";
+import setupCompanies from "../filters/companies.js";
+import setupPrice from "../filters/price.js";
 
 // importações especificas
 
-import {store, setupStore} from '../store.js'
-import display from '../displayProducts.js'
-import {getElement} from '../util.js'
+import { store, setupStore } from "../store.js";
+import display from "../displayProducts.js";
+import { getElement } from "../util.js";
 
+const init = async () => {
+  const loading = getElement(".page-loading");
+  if (store.length < 1) {
+    const products = await fetchProducts();
+    setupStore(products);
+  }
+  display(store, getElement(".products-container"));
+};
