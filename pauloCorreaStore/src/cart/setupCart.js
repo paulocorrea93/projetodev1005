fa5a8975e793e8e7516643e9feb0fee654fd475f
Paulo <paulocorreapc93@gmail.com>
2023-06-19 11:10:51 -0400

    criado evento para fechar carrinho de compras

--- pauloCorreaStore/src/cart/setupCart.js
+++ pauloCorreaStore/src/cart/setupCart.js
@@ -7,3 +7,7 @@ const toggleCartBtn = getElement('.toggle-cart')
 toggleCartBtn.addEventListener('click', () => {
     cartOverlay.classList.add('show')
 })
+
+closeCartBtn.addEventListener('click', () => {
+    cartOverlay.classList.remove('show')
+})
