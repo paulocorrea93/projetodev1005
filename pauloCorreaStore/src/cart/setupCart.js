789052fb15935128c77ab295123ff6da181bef7a
Paulo <paulocorreapc93@gmail.com>
2023-06-19 11:11:51 -0400

    versão 1.0 da lógica dá página 'setupCart' finalizada

--- pauloCorreaStore/src/cart/setupCart.js
+++ pauloCorreaStore/src/cart/setupCart.js
@@ -1,13 +1,17 @@
-import {getElement} from '../util.js'
+import { getElement } from "../util.js";
 
-const cartOverlay = getElement('.cart-overlay')
-const closeCartBtn = getElement('.cart-close')
-const toggleCartBtn = getElement('.toggle-cart')
+const cartOverlay = getElement(".cart-overlay");
+const closeCartBtn = getElement(".cart-close");
+const toggleCartBtn = getElement(".toggle-cart");
 
-toggleCartBtn.addEventListener('click', () => {
-    cartOverlay.classList.add('show')
-})
+toggleCartBtn.addEventListener("click", () => {
+  cartOverlay.classList.add("show");
+});
 
-closeCartBtn.addEventListener('click', () => {
-    cartOverlay.classList.remove('show')
-})
+closeCartBtn.addEventListener("click", () => {
+  cartOverlay.classList.remove("show");
+});
+
+export const openCart = () => {
+  cartOverlay.classList.add("show");
+};
